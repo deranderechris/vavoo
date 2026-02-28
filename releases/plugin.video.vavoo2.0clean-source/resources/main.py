17317a69072556fbcc467edc1d92987b2d017e40
derandere <85012482+deranderechris@users.noreply.github.com>
2026-02-28 23:03:46 +0000

    Lokale Änderungen vor Pull gesichert

---
+++ releases/plugin.video.vavoo2.0clean-source/resources/main.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+# edit by der andere
+
+if __name__ == "__main__":
+    from vavoo.utils import *
+    from vavoo.epg import service as epg
+    from vavoo import vjackson, vavoo_tv, vjlive
+    joyn_token=translatePath("special://profile/addon_data/plugin.video.joyn/data/auth_tokens")
+    if exists(joyn_token):
+        try:
+            with open(joyn_token) as k:
+                joyn_token_json = json.load(k)
+            if joyn_token_json["expires_in"] == 86400000:
+                joyn_token_json["expires_in"] = 86400
+                with open(joyn_token, "w") as k:
+                    json.dump(joyn_token_json, k)
+        except:pass
+    params = dict(parse_qsl(sys.argv[2][1:]))
+    tv = params.get("name")
+    action = params.pop("action", None)
+    if tv:
+        if action == "addTvFavorit": vjlive.change_favorit(tv)
+        elif action == "delTvFavorit": vjlive.change_favorit(tv, True)
+        else: vjlive.livePlay(tv, params.get('type'), params.get('group'))
+    elif action == None: vjackson.menu(params)
+    elif action == "choose": vavoo_tv.choose()
+    # Stalker-Logik entfernt
+    elif action == "clear": clear()
+    elif action == "delete_search": delete_search(params)
+    elif action == "delallTvFavorit":
+        setSetting("favs", "[]")
+        execute('Container.Refresh')
+    # edit by der andere
+    elif action == "channels": vjlive.channels(params.get('items'), params.get('type'), params.get('group'))
+    elif action == "settings": openSettings(sys.argv[1])
+    elif action == "favchannels": vjlive.favchannels()
+    elif action == "makem3u": vjlive.makem3u()
+    #else: getattr(vjackson, action)(params)
